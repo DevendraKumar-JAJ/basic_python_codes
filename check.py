42d2f7ce6f08ca5c6fb5f63729735f9cbd8f6e5e
Devendra Kumar <dkumar06545@gmail.com>
2025-05-16 06:37:49 +0530

    New branch

---
+++ check.py
@@ -0,0 +1,11 @@
+import sys
+print('Name '*3)
+# print name 3 times...
+
+def mul(name,times=0):
+  return name*times
+
+print(mul('Dev ',5))
+
+thislist = ["apple", 25,True,4j]
+[print (type(x)) for x in thislist] # printing type of all list  mambers 
